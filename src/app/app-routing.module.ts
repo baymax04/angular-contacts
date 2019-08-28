e481b6ce3569b0088d8217cc0c77d5e2c26b87db
wangyl <2297702505@qq.com>
2019-08-28 23:24:26 +0800

    20190828 23:24 配置路由导航

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,27 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { SigninComponent } from './signin/signin.component';
+import { SignupComponent } from './signup/signup.component';
 
+// 创建路由表
+const routes: Routes = [
+  {
+    path: 'signin',
+    component: SigninComponent
+  },
+  {
+    path: 'signup',
+    component: SignupComponent
+  }
+];
+
+// 0.路由模块初始化
+// 1.配置路由表
+// 请求 xxx 路径的时候，导航到某给组件
+// 2.配置路由出口，及路由导航链接
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes)],  // routes路由表 被作用于路由模块
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
